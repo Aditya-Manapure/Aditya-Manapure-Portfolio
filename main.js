f0742e7fb0e2a55546eb3f285bd2e13cd6b7f9eb
Aditya Manapure <73328745+Aditya-Manapure@users.noreply.github.com>
2021-12-05 14:45:53 +0530

    updated

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-// Sticky Navbar
 let header = document.querySelector('header');
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
 
 window.addEventListener('scroll', () => {
     header.classList.toggle('shadow', window.scrollY > 0);
 });
 
-menu.onclick = () => {
-    navbar.classList.toggle('active');
-}
-window.onscroll = () => {
-    navbar.classList.remove('active');
-}
+
+function validateForm() {
+    let fname = document.forms["myForm"]["Name"].value;
+    let mail = document.forms["myForm"]["email"].value;
+    let desc = document.forms["myForm"]["decs"].value;
+
+    if (fname == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+    else if (mail == "")
+    {
+        alert("Email must be filled out");
+        return false;
+    }
+    else if(desc == "")
+    {
+        alert("Description must be filled out");
+        return false;
+    }
+  }
+
+
+
 
